66b88e97a78a08c8932235d1251fd5aa9a01805f
h1326432154 <m15191488150@163.com>
2020-04-14 17:49:12 +0800

    链表 两数之和

--- leetcode-go/sum2_test.go
+++ leetcode-go/sum2_test.go
@@ -1,6 +1,7 @@
 package leetcode
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -47,7 +48,7 @@ func TestSumTwoNumber(t *testing.T) {
 	t.Log(res.Next.Next.Val)
 }
 
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+func addTwoNumbers1(l1 *ListNode, l2 *ListNode) *ListNode {
 	var result *ListNode
 	next := &result
 	num := 0
@@ -68,4 +69,41 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		next = &((*next).Next)
 	}
 	return result
+}
+
+// 递归方式
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var a, b []int
+	for p := l1; p != nil; p = p.Next {
+		a = append(a, p.Val)
+	}
+	for p := l2; p != nil; p = p.Next {
+		b = append(b, p.Val)
+	}
+	fmt.Println("a =", a)
+	fmt.Println("a =", b)
+	var res *ListNode
+	carry := 0
+	aLen, bLen := len(a), len(b)
+	for i, j := 0, 0; i < aLen || j < bLen; i, j = i+1, j+1 {
+		sum := carry
+		if i < aLen {
+			sum += a[aLen-i-1]
+		}
+		if j < bLen {
+			sum += b[bLen-j-1]
+		}
+		node := ListNode{Val: sum % 10, Next: nil}
+		if res == nil {
+			res = &node
+		} else {
+			node.Next = res
+			res = &node
+		}
+		carry = sum / 10
+	}
+	if carry != 0 {
+		res = &ListNode{Val: carry, Next: res}
+	}
+	return res
 }
